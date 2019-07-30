e3a464bbb8f1765540b5a1ec7eeeeb44561dd510
mwild96 <44030849+mwild96@users.noreply.github.com>
2019-07-30 11:23:00 +0100

    Add files via upload

--- embedder.py
+++ embedder.py
@@ -7,6 +7,8 @@ Created on Thu Jun 13 14:14:47 2019
 
 import numpy as np
 import nltk.tokenize 
+import gensim.utils
+import gensim.models
 import collections
 import tqdm
 import re
@@ -269,3 +271,43 @@ def bert_input_builder(string, tokenizer, max_tokens, padding = True):
         input_mask += padding
         
     return indexed_tokens, segments, input_mask
+
+
+
+
+#https://stackoverflow.com/questions/45981305/convert-python-dictionary-to-word2vec-object
+def my_save_word2vec_format(fname, vocab, vectors, binary=True, total_vec=2):
+    """Store the input-hidden weight matrix in the same format used by the original
+    C word2vec-tool, for compatibility.
+    
+    Parameters
+    ----------
+    fname : str
+        The file path used to save the vectors in.
+    vocab : dict
+        The vocabulary of words.
+    vectors : numpy.array
+        The vectors to be stored.
+    binary : bool, optional
+        If True, the data wil be saved in binary word2vec format, else it will be saved in plain text.
+    total_vec : int, optional
+        Explicitly specify total number of vectors
+        (in case word vectors are appended with document vectors afterwards).
+    
+    """
+    if not (vocab or vectors):
+        raise RuntimeError("no input")
+    if total_vec is None:
+        total_vec = len(vocab)
+    vector_size = vectors.shape[1]
+    assert (len(vocab), vector_size) == vectors.shape
+    with gensim.utils.smart_open(fname, 'wb') as fout:
+        print(total_vec, vector_size)
+        fout.write(gensim.utils.to_utf8("%s %s\n" % (total_vec, vector_size)))
+        # store in sorted order: most frequent words at the top
+        for word, row in vocab.items():
+            if binary:
+                row = row.astype('float32')
+                fout.write(gensim.utils.to_utf8(word) + b" " + row.tostring())
+            else:
+                fout.write(gensim.utils.to_utf8("%s %s\n" % (word, ' '.join(repr(val) for val in row))))
