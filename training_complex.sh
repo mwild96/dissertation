3663fd222ababd92c6d89c038961ba49e283c706
mwild96 <44030849+mwild96@users.noreply.github.com>
2019-08-12 12:33:04 +0100

    Update training_complex.sh

--- training_complex.sh
+++ training_complex.sh
@@ -1,5 +1,5 @@
 #adapted from the repository for the University of Edinburgh School of Informatics Machine Learning Practical course:
-#https://github.com/CSTR-Edinburgh/mlpractical
+#https://github.com/CSTR-Edinburgh/mlpractical/tree/mlp2018-9/mlp_cluster_tutorial
 
 #!/bin/sh
 #SBATCH -N 1	  # nodes requested
