d665cfc733b7949a25f468d52944f66568a4c3e1
mwild96 <44030849+mwild96@users.noreply.github.com>
2019-08-12 12:13:27 +0100

    Update arg_extractor.py

--- arg_extractor.py
+++ arg_extractor.py
@@ -1,4 +1,5 @@
-#obviously all of this code was stolen from the mlp examples/tutorials
+#adapted from the repository for the University of Edinburgh School of Informatics Machine Learning Practical course:
+#https://github.com/CSTR-Edinburgh/mlpractical
 
 import argparse
 import json
@@ -28,7 +29,6 @@ def get_args():
     parser.add_argument('--val_dataset_name', type=str, help='Dataset on which the system will evaluate our model')
     parser.add_argument('--data_path', type=str, help='the path to pull the trian and validation datasets from')
     parser.add_argument('--output_path', type=str, help='path for the model checkpoint file to go')
-    
     parser.add_argument('--use_gpu', nargs="?", type=str2bool, default=True,
                         help='A flag indicating whether we will use GPU acceleration or not')
     parser.add_argument('--gpu_id', type=str, default="None", help="A string indicating the gpu to use")
@@ -39,60 +39,35 @@ def get_args():
     parser.add_argument('--D', type=int, help='feature dimension')
     parser.add_argument('--train_size', type=int, help='number of observations in the train data set')
     parser.add_argument('--val_size', type=int, help='number of observations in the validation data set')
-    parser.add_argument('--output_filename', type=str, help='the base name of the file for the model checkpoints to be outputted as')
-    parser.add_argument('--network', type=str, help='the name of the type of network we want to use, must be one from the mynetworks.py file')
-    parser.add_argument('--dataset_class', type=str, help='the name of the type of dataset we are using, must be one from the architectures.py file')
-    
-    
-    
-    parser.add_argument('--embedding_type', type=str, default = None, help='the name of the embedding model to be used, only relevant for non-SIF models')
-    parser.add_argument('--max_tokens', type=int, default = None, help='the maximum number of tokens to include for padding purposes, only relevant for non-SIF models')
-    parser.add_argument('--text_column1', type=str, default=None, help='the name of the column of text data from the first dataset in the pair of datasets to be linked, only relevant for non-SIF models')
-    parser.add_argument('--text_column2', type=str, default=None, help='the name of the column of text data from the second dataset in the pair of datasets to be linked, only relevant for non-SIF models')
-    parser.add_argument('--encoding', type=str, default=None, help='the type of encoding to be used when importing the raw text dataset')
-    parser.add_argument('--hidden_size', type=int, default=None, help='the size of the hidden layers to be used in the LSTM encoder, only relevant for non-SIF models')
-    parser.add_argument('--num_layers', type=int, default=None, help='the number of layers to be used in the LSTM encoder, only relevant for non-SIF models')
-    parser.add_argument('--out_features', type=int, default=None, help='tbh idk')
+    parser.add_argument('--output_filename', type=str, 
+                        help='the base name of the file for the model checkpoints to be outputted as')
+    parser.add_argument('--network', type=str, 
+                        help='the name of the type of network we want to use, must be one from the mynetworks.py file')
+    parser.add_argument('--dataset_class', type=str, 
+                        help='the name of the type of dataset we are using, must be one from the architectures.py file')
+    parser.add_argument('--embedding_type', type=str, default = None, 
+                        help='the name of the embedding model to be used, only relevant for non-SIF models')
+    parser.add_argument('--max_tokens', type=int, default = None, 
+                        help='the maximum number of tokens to include for padding purposes, only relevant for non-SIF models')
+    parser.add_argument('--text_column1', type=str, default=None, 
+                        help='the name of the column of text data from the first dataset in the pair of datasets to be linked, only relevant for non-SIF models')
+    parser.add_argument('--text_column2', type=str, default=None, 
+                        help='the name of the column of text data from the second dataset in the pair of datasets to be linked, only relevant for non-SIF models')
+    parser.add_argument('--encoding', type=str, default=None, 
+                        help='the type of encoding to be used when importing the raw text dataset')
+    parser.add_argument('--hidden_size', type=int, default=None, 
+                        help='the size of the hidden layers to be used in the LSTM encoder, only relevant for non-SIF models')
+    parser.add_argument('--num_layers', type=int, default=None, 
+                        help='the number of layers to be used in the LSTM encoder, only relevant for non-SIF models')
+    parser.add_argument('--out_features', type=int, default=None, 
+                        help='the size of the output of Feed-Forward layer of the LSTM-Siamese Net')
     parser.add_argument('--dropout', type=float, default=None, help='dropout probability to be used in the LSTM encoder')
-    parser.add_argument('--bidirectional', type=bool, default=None, help='whether or not the LSTM encoder should be bidirectional, only relevant for non-SIF models')
-                     
-    
-    #do I need this shit?
-#     parser.add_argument('--filepath_to_arguments_json_file', nargs="?", type=str, default=None,help='')
-    
-    
-    
-    
-    
-    
-#     parser.add_argument('--continue_from_epoch', nargs="?", type=int, default=-1, help='Batch_size for experiment')
-#     parser.add_argument('--dataset_name', type=str, help='Dataset on which the system will train/eval our model')
-#     parser.add_argument('--seed', nargs="?", type=int, default=7112018,
-#                         help='Seed to use for random number generator for experiment')
-#     parser.add_argument('--image_num_channels', nargs="?", type=int, default=1,
-#                         help='The channel dimensionality of our image-data')
-#     parser.add_argument('--image_height', nargs="?", type=int, default=28, help='Height of image data')
-#     parser.add_argument('--image_width', nargs="?", type=int, default=28, help='Width of image data')
-#     parser.add_argument('--dim_reduction_type', nargs="?", type=str, default='strided_convolution',
-#                         help='One of [strided_convolution, dilated_convolution, max_pooling, avg_pooling]')
-#     parser.add_argument('--num_layers', nargs="?", type=int, default=4,
-#                         help='Number of convolutional layers in the network (excluding '
-#                              'dimensionality reduction layers)')
-#     parser.add_argument('--num_filters', nargs="?", type=int, default=64,
-#                         help='Number of convolutional filters per convolutional layer in the network (excluding '
-#                              'dimensionality reduction layers)')
-#     parser.add_argument('--num_epochs', nargs="?", type=int, default=100, help='The experiment\'s epoch budget')
-#     parser.add_argument('--experiment_name', nargs="?", type=str, default="exp_1",
-#                         help='Experiment name - to be used for building the experiment folder')
-
-
+    parser.add_argument('--bidirectional', type=bool, default=None, 
+                        help='whether or not the LSTM encoder should be bidirectional, only relevant for non-SIF models')
 
     args = parser.parse_args()
     gpu_id = str(args.gpu_id)
-#     if args.filepath_to_arguments_json_file is not None:
-#         args = extract_args_from_json(json_file_path=args.filepath_to_arguments_json_file, existing_args_dict=args)
-
-                  
+     
     if gpu_id != "None":
         args.gpu_id = gpu_id
 
@@ -143,23 +118,3 @@ def get_args():
         device = torch.device('cpu')  # sets the device to be CPU
 
     return args, device
-
-
-# class AttributeAccessibleDict(object):
-#     def __init__(self, adict):
-#         self.__dict__.update(adict)
-
-
-# def extract_args_from_json(json_file_path, existing_args_dict=None):
-
-#     summary_filename = json_file_path
-#     with open(summary_filename) as f:
-#         arguments_dict = json.load(fp=f)
-
-#     for key, value in vars(existing_args_dict).items():
-#         if key not in arguments_dict:
-#             arguments_dict[key] = value
-
-#     arguments_dict = AttributeAccessibleDict(arguments_dict)
-
-# return arguments_dict
